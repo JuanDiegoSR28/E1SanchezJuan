aa9d46358040a6114a8abb163c39576a04bf938d
JuanDiegoSR28 <juan.sanchez233013@potros.itson.edu.mx>
2024-06-07 14:48:14 -0700

    Proyecto finalizado.
    Estado final del proyecto.

--- E1SanchezJuan/app/src/main/java/mx/itson/edu/e1SanchezJuan/MainActivity.kt
+++ E1SanchezJuan/app/src/main/java/mx/itson/edu/e1SanchezJuan/MainActivity.kt
@@ -7,76 +7,114 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var percentage : String
-        val originalPrice : EditText = findViewById<EditText>(R.id.OriginalPrice)
-        val percentage01 : Button = findViewById(R.id.First_Percentage)
-        val percentage02 : Button = findViewById(R.id.Second_Percentage)
-        val percentage03 : Button = findViewById(R.id.Third_Percentage)
-        val percentage04 : Button = findViewById(R.id.Forth_Percentage)
-        val percentage05 : Button = findViewById(R.id.Fifth_Percentage)
-        val percentage06 : Button = findViewById(R.id.Sixth_Percentage)
-        val displayValorPercentage : TextView = findViewById<TextView>(R.id.PlaceHolderPercentage)
-
-        percentage01.setOnClickListener()
-        {
-
-            var priceToDouble = percentage01.text.toString().toDoubleOrNull() as Double
-                percentage = priceToDouble*0.1
-                displayValorPercentage.setText(percentage)
+        val entradaPrecio: EditText = findViewById(R.id.OriginalPrice)
+        val percentage01: Button = findViewById(R.id.First_Percentage)
+        val percentage02: Button = findViewById(R.id.Second_Percentage)
+        val percentage03: Button = findViewById(R.id.Third_Percentage)
+        val percentage04: Button = findViewById(R.id.Forth_Percentage)
+        val percentage05: Button = findViewById(R.id.Fifth_Percentage)
+        val percentage06: Button = findViewById(R.id.Sixth_Percentage)
+        val tip: Button = findViewById(R.id.Button_Tip)
+        val discount: Button = findViewById(R.id.Button_Discount)
+        val displayValorPercentage: TextView = findViewById(R.id.PlaceHolderPercentage)
+        val displayValorTotal: TextView = findViewById(R.id.PlaceHolderTotal)
 
 
-        }
-
-        percentage02.setOnClickListener()
-        {
-            var priceToDouble = percentage02.text.toString().toDoubleOrNull() as Double
-            percentage = priceToDouble*0.15
-            displayValorPercentage.setText(percentage)
-
-        }
-
-        percentage03.setOnClickListener()
-        {
-            var priceToDouble = percentage01.text.toString().toDoubleOrNull() as Double
-            percentage = priceToDouble*0.2
-            displayValorPercentage.setText(percentage)
-
-        }
-
-        percentage04.setOnClickListener()
-        {
-            var priceToDouble = percentage01.text.toString().toDoubleOrNull() as Double
-            percentage = priceToDouble*0.25
-            displayValorPercentage.setText(percentage)
-
-        }
-
-        percentage05.setOnClickListener()
-        {
-            var priceToDouble = percentage01.text.toString().toDoubleOrNull() as Double
-            percentage = priceToDouble*0.30
-            displayValorPercentage.setText(percentage)
-
-        }
-
-        percentage06.setOnClickListener()
-        {
-            var priceToDouble = percentage01.text.toString().toDoubleOrNull() as Double
-            percentage = priceToDouble*0.4
-            displayValorPercentage.setText(percentage)
-
+        percentage01.setOnClickListener {
+            val entradaPrecioString = entradaPrecio.text.toString()
+            val entradaPrecioToDouble = entradaPrecioString.toDoubleOrNull()
+            entradaPrecioToDouble?.let {
+                displayValorPercentage.text = calcularPorcentaje(it, 0.10).toString()
+            }
         }
 
 
+        percentage02.setOnClickListener {
+            val entradaPrecioString = entradaPrecio.text.toString()
+            val entradaPrecioToDouble = entradaPrecioString.toDoubleOrNull()
+            entradaPrecioToDouble?.let {
+                displayValorPercentage.text = calcularPorcentaje(it, 0.15).toString()
+            }
+        }
 
+        percentage03.setOnClickListener {
+            val entradaPrecioString = entradaPrecio.text.toString()
+            val entradaPrecioToDouble = entradaPrecioString.toDoubleOrNull()
+            entradaPrecioToDouble?.let {
+                displayValorPercentage.text = calcularPorcentaje(it, 0.20).toString()
+            }
+        }
+
+        percentage04.setOnClickListener {
+            val entradaPrecioString = entradaPrecio.text.toString()
+            val entradaPrecioToDouble = entradaPrecioString.toDoubleOrNull()
+            entradaPrecioToDouble?.let {
+                displayValorPercentage.text = calcularPorcentaje(it, 0.25).toString()
+            }
+        }
+
+        percentage05.setOnClickListener {
+            val entradaPrecioString = entradaPrecio.text.toString()
+            val entradaPrecioToDouble = entradaPrecioString.toDoubleOrNull()
+            entradaPrecioToDouble?.let {
+                displayValorPercentage.text = calcularPorcentaje(it, 0.30).toString()
+            }
+        }
+
+        percentage06.setOnClickListener {
+            val entradaPrecioString = entradaPrecio.text.toString()
+            val entradaPrecioToDouble = entradaPrecioString.toDoubleOrNull()
+            entradaPrecioToDouble?.let {
+                displayValorPercentage.text = calcularPorcentaje(it, 0.35).toString()
+            }
+        }
+
+        tip.setOnClickListener{
+            val entradaPrecioString = entradaPrecio.text.toString()
+            val entradaPrecioToDouble = entradaPrecioString.toDoubleOrNull()
+            val entradaPorcentajeString = displayValorPercentage.text.toString()
+            val entradaPorcentajeToDouble = entradaPorcentajeString.toDoubleOrNull()
+            entradaPorcentajeToDouble?.let {
+                displayValorTotal.text = calcularTip(entradaPrecioToDouble, entradaPorcentajeToDouble).toString()
+        }
+    }
+        discount.setOnClickListener{
+            val entradaPrecioString = entradaPrecio.text.toString()
+            val entradaPrecioToDouble = entradaPrecioString.toDoubleOrNull()
+            val entradaPorcentajeString = displayValorPercentage.text.toString()
+            val entradaPorcentajeToDouble = entradaPorcentajeString.toDoubleOrNull()
+            entradaPorcentajeToDouble?.let {
+                displayValorTotal.text = calcularDescuento(entradaPrecioToDouble, entradaPorcentajeToDouble).toString()
+            }
+        }
+
+
+
+
+
+
+
+
+}
+
+    private fun calcularDescuento(entradaPrecioToDouble: Double?, entradaPorcentajeToDouble: Double):Double? {
+        if (entradaPrecioToDouble != null) {
+            return entradaPrecioToDouble - entradaPorcentajeToDouble
+        }
+        return null
     }
 
-
-
-
+    private fun calcularTip(entradaPrecioToDouble: Double?, entradaPorcentajeToDouble: Double): Double? {
+        if (entradaPrecioToDouble != null) {
+            return entradaPrecioToDouble + entradaPorcentajeToDouble
+        }
+        return null
+    }
+    private fun calcularPorcentaje(precio: Double, porcentaje: Double): Double {
+        return precio * porcentaje
+    }
 }
